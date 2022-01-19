2295e0980dd71a0d14081ef7ee1022d3b309bb94
footoredo <footoredo@gmail.com>
2022-01-18 20:21:49 -0500

    itii

--- dreamerv2/common/nets.py
+++ dreamerv2/common/nets.py
@@ -51,6 +51,7 @@ class EnsembleRSSM(common.Module):
         self._use_independent_state_transformer = config.use_independent_state_transformer and self.use_transformer
         self._use_inside_transformer = config.use_inside_transformer and self._use_independent_transformer
         self._inside_transformer_include_action = config.inside_transformer_include_action
+        self._inside_transformer_include_importance = config.inside_transformer_include_importance
         self._myopic_prediction = config.myopic_prediction
         self.use_independent_transformer_encoder = config.use_independent_transformer_encoder
         self._include_transformer_embed = config.include_transformer_embed
@@ -130,6 +131,8 @@ class EnsembleRSSM(common.Module):
                 token_size = total_stoch + self._deter
                 if self._inside_transformer_include_action:
                     token_size += self._num_actions
+                if self._inside_transformer_include_importance:
+                    token_size += 1
                 state['t_memory'] = tf.zeros([batch_size, self._inside_memory_size, token_size])
                 state['t_importance'] = tf.zeros([batch_size, self._inside_memory_size])
         return state
@@ -551,6 +554,8 @@ class EnsembleRSSM(common.Module):
                 last_importance = tf.stop_gradient(self._importance_head(last_token).mode())
                 if self._inside_transformer_include_action:
                     last_token = tf.concat([last_token, prev_action], 1)
+                if self._inside_transformer_include_importance:
+                    last_token = tf.concat([last_token, last_importance[:, tf.newaxis]], 1)
                 t_memory = tf.concat((t_memory, last_token[:, tf.newaxis, :]), 1)
                 t_importance = tf.concat((t_importance, last_importance[:, tf.newaxis]), 1)
                 _, indices = tf.math.top_k(t_importance, k=self._inside_memory_size)
